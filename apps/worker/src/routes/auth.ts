60a0f861898b966eb858a4a527bb7b89b1a25ac1
jabardast-afk <sandeepkumarbtech2012@gmail.com>
2026-03-23 14:51:13 +0000

    extra scopes removal

--- apps/worker/src/routes/auth.ts
+++ apps/worker/src/routes/auth.ts
@@ -37,7 +37,7 @@ async function startGoogleAuth(c: Context<AppBindings>) {
     redirect_uri: c.env.GOOGLE_REDIRECT_URI,
     response_type: "code",
     scope:
-      "openid email profile",
+      "openid email profile https://www.googleapis.com/auth/generative-language.retriever",
     access_type: "offline",
     prompt: "consent",
     state,
